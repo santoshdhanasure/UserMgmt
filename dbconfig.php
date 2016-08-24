8dbd78443aff64afd782d0c9c92a4d4a151d955b
santoshdhanasure <santosh_dhanasure@persistent.co.in>
2016-08-24 17:49:08 +0530

    Modified configuration file

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,24 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$datbase = "dbtuts";
-mysql_connect($host,$user,$password);
-mysql_select_db($datbase);
+error_reporting(E_ERROR | E_PARSE);
+
+$node1="NODE1_IP:3306";
+$node2="NODE2_IP:3306";
+
+$user="root";
+$pass="root";
+
+if( !mysql_connect($node1, $user, $pass) ){
+
+#if( !$conn_node1 ){
+        #echo "Node 1" . mysql_error();
+      $conn_node2 = mysql_connect($node2, $user, $pass);
+      if (!$conn_node2){
+                echo "Not been able to connect to both nodes " . mysql_error();
+      }else{
+        echo "Connected node 2";
+      }
+}else{
+        echo "Connected to node1";
+}
+
 ?>
